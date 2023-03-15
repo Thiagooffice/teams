260bb5e61f9ae5984df7a793a77681b8b4819b45
Thiago <thiagooffice2@gmail.com>
2023-03-15 15:24:57 -0300

    feat: creating input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,7 +1,8 @@
-import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
+import { Input } from '@components/Input'
 import {Container, Content, Icon} from './styles'
+import { Button } from '@components/Button'
 
 export function NewGroup(){
     return(
@@ -15,8 +16,11 @@ export function NewGroup(){
                 subtitle='Cria uma turma para adicionar as pessoas'
                 />
 
+                <Input/>
+
                 <Button
                 title='Criar'
+                style={{marginTop: 20}}
                 />
             </Content>
         </Container>
