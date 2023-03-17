3db8c99d55b5a7c0cf5344720080609d7fd0791c
Thiago <thiagooffice2@gmail.com>
2023-03-17 19:42:30 -0300

    feat: creating routes page

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import theme from './src/theme';
 
-import { Groups } from '@screens/Groups';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         translucent={true}
       />
       {
-        fontsLoaded ? <Groups /> : <Loading />
+        fontsLoaded ? <Routes /> : <Loading />
       }
     </ThemeProvider>
   );
