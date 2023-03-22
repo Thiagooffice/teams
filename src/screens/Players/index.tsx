e4d962930676f9df6d4e3ac8696a4c77646f95b4
Thiago <thiagooffice2@gmail.com>
2023-03-22 15:42:04 -0300

    feat: Carregando Players pelo Grupo e Time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -4,12 +4,16 @@ import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { ButtonIcon } from '@components/ButtonIcon'
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 import { useRoute } from "@react-navigation/native";
+import { AppError } from "@utils/AppError";
+import { playerAddByGroup } from "@storage/player/playerAddByGroup";
+import { playersGetByGroupAndTeam } from "@storage/player/playerGetByGroupAndTeam";
+import { PlayerStorageDTO } from "@storage/player/PlayerStorageDTO";
 
 type RouteParams = {
     group: string
@@ -17,11 +21,44 @@ type RouteParams = {
 
 
 export function Players() {
+    const [newPlayerName, setNewPlayerName] = useState("")
     const [team, setTeam] = useState("Time A")
-    const [players, setPlayers] = useState([])
+    const [players, setPlayers] = useState<PlayerStorageDTO[]>([])
 
     const route = useRoute();
     const { group } = route.params as RouteParams
+
+    async function handleAddPlayer() {
+        if (newPlayerName.trim().length === 0) {
+            return Alert.alert("Nova pessoa", "Informe o nome da pessoa para adicionar")
+        }
+
+        const newPlayer = {
+            name: newPlayerName,
+            team,
+        }
+
+        try {
+            await playerAddByGroup(newPlayer, group);
+        } catch (error) {
+            if (error instanceof AppError) {
+                Alert.alert("Nova pessoa", error.message)
+            } else {
+                console.log(error);
+                Alert.alert("Nova pessoa", "Não foi possível adicionar")
+            }
+        }
+    }
+
+    async function fetchPlayersByTeam(){
+        try {
+            const playersbyTeam = await playersGetByGroupAndTeam(group, team)
+            setPlayers(playersbyTeam)
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Pessoas", "Não foi possível carregar as pessoas filtradas do time selecionado")
+        }
+    }
 
     return (
         <Container>
@@ -35,9 +72,11 @@ export function Players() {
                 <Input
                     placeholder="Nome da pessoa"
                     autoCorrect={false}
+                    onChangeText={setNewPlayerName}
                 />
                 <ButtonIcon
                     icon="add"
+                    onPress={handleAddPlayer}
                 />
             </Form>
 
